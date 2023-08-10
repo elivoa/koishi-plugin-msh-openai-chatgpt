6bb124550b634eba65834d22e5c6720178553c6a
高博 <gaobo@rcrai.com>
2023-08-10 18:17:09 +0800

    v1.3.1

--- src/index.ts
+++ src/index.ts
@@ -23,13 +23,13 @@ export const Config: Schema<Config> = Schema.object({
   apiKey: Schema.string().required().description("OpenAI API Key: https://platform.openai.com/account/api-keys"),
   apiAddress: Schema.string().required().default("https://api.openai.com/v1").description("API 请求地址。"),
   triggerWord: Schema.string().default("chat").description("触发机器人回答的关键词。"),
-  model: Schema.union(['gpt-3.5-turbo', 'gpt-3.5-turbo-0301']).default('gpt-3.5-turbo'),
+  model: Schema.union(['gpt-3.5-turbo', 'gpt-3.5-turbo-0301', 'MSH-L']).default('gpt-3.5-turbo'),
   temperature: Schema.number().default(1).description("温度，更高的值意味着模型将承担更多的风险。对于更有创造性的应用，可以尝试 0.9，而对于有明确答案的应用，可以尝试 0（argmax 采样）。"),
   maxTokens: Schema.number().default(100).description("生成的最大令牌数。"),
   topP: Schema.number().default(1),
   frequencyPenalty: Schema.number().default(0).description('数值在 -2.0 和 2.0 之间。正值是根据到目前为止它们在文本中的现有频率来惩罚新的标记，减少模型逐字逐句地重复同一行的可能性。'),
   presencePenalty: Schema.number().default(0).description('数值在 -2.0 和 2.0 之间。正值根据新标记在文本中的现有频率对其进行惩罚，减少了模型（model）逐字重复同一行的可能性。'),
-  stop: Schema.array(Schema.string()).default(null).description('生成的文本将在遇到任何一个停止标记时停止。'),
+  stop: Schema.array(Schema.string()).default([]).description('生成的文本将在遇到任何一个停止标记时停止。'),
   errorMessage: Schema.string().default("回答出错了，请联系管理员。").description("回答出错时的提示信息。"),
   pictureMode: Schema.boolean().default(false).description("开启图片模式。")
 })
@@ -67,18 +67,44 @@ export async function apply(ctx: Context, config: Config) {
     }
   })
 
-  ctx.on('message', async (session) => {
-    const q = session.content;
-    session.send('我是复读机4：' + session.content)
-    // if (session.content === '天王盖地虎') {
-    //   session.send('宝塔镇河妖')
-    // }
+  // ctx.on('message', async (session) => {
+  //   const q = session.content;
+  //   // session.send('我是复读机4：' + session.content)
+  //   // if (session.content === '天王盖地虎') {
+  //   //   session.send('宝塔镇河妖')
+  //   // }
+  //   console.log(".........debug onmessage:", q);
+  //   session.send("查询中，请耐心等待..." + session.content);
+  //   // try {
+  //   //   const completion = await openai.createChatCompletion({
+  //   //     model: config.model,
+  //   //     messages: [{ "role": "user", 'content': q }],
+  //   //     temperature: config.temperature,
+  //   //     max_tokens: config.maxTokens,
+  //   //     top_p: config.topP,
+  //   //     frequency_penalty: config.frequencyPenalty,
+  //   //     presence_penalty: config.presencePenalty,
+  //   //     stop: config.stop,
+  //   //   });
+  //   //   return completion.data.choices[0].message.content;
+  //   // } catch (error) {
+  //   //   if (error.response) {
+  //   //     console.log(error.response.status);
+  //   //     console.log(error.response.data);
+  //   //   } else {
+  //   //     console.log(error.message);
+  //   //   }
+  //   //   return config.errorMessage;
+  //   // }
 
+  // })
+
+  ctx.middleware(async (session, next) => {
     session.send("查询中，请耐心等待...");
     try {
       const completion = await openai.createChatCompletion({
         model: config.model,
-        messages: [{ "role": "user", 'content': q }],
+        messages: [{ "role": "user", 'content': session.content }],
         temperature: config.temperature,
         max_tokens: config.maxTokens,
         top_p: config.topP,
@@ -97,31 +123,36 @@ export async function apply(ctx: Context, config: Config) {
       return config.errorMessage;
     }
 
+    // return next()
+    return 'TODO 解开我' + session.content
   })
 
+
   // ctx.command('chat <message:text>').action(async ({ session }, message) => {
+  //   console.log(".........debug on chat:", message);
   //   const q = message;
-  //   session.send("查询中，请耐心等待...");
-  //   try {
-  //     const completion = await openai.createChatCompletion({
-  //       model: config.model,
-  //       messages: [{ "role": "user", 'content': q }],
-  //       temperature: config.temperature,
-  //       max_tokens: config.maxTokens,
-  //       top_p: config.topP,
-  //       frequency_penalty: config.frequencyPenalty,
-  //       presence_penalty: config.presencePenalty,
-  //       stop: config.stop,
-  //     });
-  //     return completion.data.choices[0].message.content;
-  //   } catch (error) {
-  //     if (error.response) {
-  //       console.log(error.response.status);
-  //       console.log(error.response.data);
-  //     } else {
-  //       console.log(error.message);
-  //     }
-  //     return config.errorMessage;
-  //   }
+  //   session.send("命中了啥奇怪的东西？" + q);
+  //   // session.send("查询中，请耐心等待...");
+  //   //   try {
+  //   //     const completion = await openai.createChatCompletion({
+  //   //       model: config.model,
+  //   //       messages: [{ "role": "user", 'content': q }],
+  //   //       temperature: config.temperature,
+  //   //       max_tokens: config.maxTokens,
+  //   //       top_p: config.topP,
+  //   //       frequency_penalty: config.frequencyPenalty,
+  //   //       presence_penalty: config.presencePenalty,
+  //   //       stop: config.stop,
+  //   //     });
+  //   //     return completion.data.choices[0].message.content;
+  //   //   } catch (error) {
+  //   //     if (error.response) {
+  //   //       console.log(error.response.status);
+  //   //       console.log(error.response.data);
+  //   //     } else {
+  //   //       console.log(error.message);
+  //   //     }
+  //   //     return config.errorMessage;
+  //   //   }
   // })
 }
